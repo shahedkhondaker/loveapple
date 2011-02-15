ba20c8ac3f0db6d17b8f8a62c85ddaf68f028394
hao0323 <hao0323@gmail.com>
2011-02-15 03:04:42 +0000

    会員情報操作サービスを追加

---
+++ src/server/src/main/java/cn/loveapple/service/cool/service/MemberCoreService.java
@@ -0,0 +1,57 @@
+package cn.loveapple.service.cool.service;
+
+import cn.loveapple.service.cool.model.LoveappleMemberModel;
+
+import com.google.appengine.api.datastore.Key;
+
+
+
+/**
+ * 認証、会員情報更新/削除など、直接{@linkplain LoveappleMemberModel Loveapple会員情報}に対して
+ * 操作を行うロジック処理を行うサービス。
+ * 
+ * @author hao_shunri
+ *
+ */
+public interface MemberCoreService {
+	/**
+	 * Loveapple会員の認証処理を行う。
+	 * 
+	 * @see #findByLoginId(String) 
+	 * @param loginId ログインID
+	 * @param password MD5で暗号化したパスワード
+	 * @return 認証が失敗した場合、<code>null</code>を返す。
+	 */
+	public LoveappleMemberModel authenticateLoveappleMember(String loginId, String password);
+	
+	/**
+	 * 新たな会員情報の登録、又は更新を行う。
+	 * 
+	 * @param member 登録、又は更新しようとする会員情報
+	 * @return 登録、又は更新した会員情報を戻す。
+	 */
+	public LoveappleMemberModel newAndPut(LoveappleMemberModel member);
+	
+	/**
+	 * キーをもとにデータを検索する
+	 * @param keyId
+	 * @return
+	 */
+	public LoveappleMemberModel queryByKey(Long keyId);
+	
+	/**
+	 * ログインIDをもとに、{@linkplain LoveappleMemberModel Loveapple会員情報}を取得する。
+	 * 
+	 * @param loginId ログインID
+	 * @return 見つからない場合、<code>null</code>を返す。
+	 */
+	public LoveappleMemberModel findByLoginId(String loginId);
+	/**
+	 * 
+	 * キーをもとにデータを検索する
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public LoveappleMemberModel queryByKey(Key key);
+}
