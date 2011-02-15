a0b3b6e6d1ea3600e48f0b4ba3b0b20a9595db33
hao0323@gmail.com <hao0323@gmail.com@fe9ab1a0-4753-0410-a5e7-0b77550f250c>
2011-02-15 03:05:40 +0000

    会員情報操作サービスを追加

---
+++ trunk/src/server/src/main/java/cn/loveapple/service/cool/service/impl/MemberCoreServiceImpl.java
@@ -0,0 +1,97 @@
+package cn.loveapple.service.cool.service.impl;
+
+import static cn.loveapple.service.cool.model.ModelConstant.*;
+
+import java.util.Date;
+
+import org.apache.commons.lang.StringUtils;
+import org.slim3.datastore.Datastore;
+import org.springframework.stereotype.Component;
+
+import cn.loveapple.service.cool.meta.LoveappleMemberModelMeta;
+import cn.loveapple.service.cool.model.LoveappleMemberModel;
+import cn.loveapple.service.cool.service.MemberCoreService;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+/**
+ * 
+ * 
+ * @author hao_shunri
+ * @since 2011/02/15
+ * @version $Revision$
+ */
+@Component
+public class MemberCoreServiceImpl implements MemberCoreService {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public LoveappleMemberModel authenticateLoveappleMember(String loginId,
+			String password) {
+		if(StringUtils.isEmpty(loginId) || StringUtils.isEmpty(password)){
+			throw new IllegalArgumentException("loginId or password is empty.");
+		}
+		LoveappleMemberModel member = findByLoginId(loginId);
+		if(member != null && password.equals(member.getPassword())){
+			return member;
+		}
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public LoveappleMemberModel newAndPut(LoveappleMemberModel member) {
+		if(member == null){
+			throw new IllegalArgumentException("member info is empty.");
+		}
+		if(member != null){
+			LoveappleMemberModel result = findByLoginId(member.getLoginId());
+			member.setKey(result.getKey());
+			member.setUpdateDate(new Date());
+		}
+		Key key = Datastore.put(member);
+		
+		return queryByKey(key);
+	}
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 */
+	public LoveappleMemberModel findByLoginId(String loginId){
+
+		if(StringUtils.isEmpty(loginId)){
+			throw new IllegalArgumentException("loginId is empty.");
+		}
+		LoveappleMemberModelMeta meta = LoveappleMemberModelMeta.get();
+		return Datastore.query(LoveappleMemberModel.class).filter(meta.loginId.equal(loginId)).asSingle();
+	}
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public LoveappleMemberModel queryByKey(Long keyId) {
+
+		if(keyId == null){
+			throw new IllegalArgumentException("keyId is empty.");
+		}
+		
+		Key key = KeyFactory.createKey(LOVEAPPLE_MEMBER_MODEL, keyId);
+		return queryByKey(key);
+	}
+
+	@Override
+	public LoveappleMemberModel queryByKey(Key key) {
+		if(key == null){
+			throw new IllegalArgumentException("key is empty.");
+		}
+		
+		LoveappleMemberModelMeta meta = LoveappleMemberModelMeta.get();
+		return Datastore.query(LoveappleMemberModel.class).filter(meta.key.equal(key)).asSingle();
+	}
+
+}
