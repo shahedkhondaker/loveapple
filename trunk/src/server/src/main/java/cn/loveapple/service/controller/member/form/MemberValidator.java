3293eff894dfbbf7aa693737eda2764341a75a3d
hao0323@gmail.com <hao0323@gmail.com@fe9ab1a0-4753-0410-a5e7-0b77550f250c>
2011-02-17 08:05:44 +0000

    エラーメッセージ機能対応

---
+++ trunk/src/server/src/main/java/cn/loveapple/service/controller/member/form/MemberValidator.java
@@ -0,0 +1,41 @@
+package cn.loveapple.service.controller.member.form;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+/**
+ * TODO
+ * 
+ * @see http://static.springsource.org/spring/docs/3.1.0.M1/spring-framework-reference/html/validation.html#validation-mvc-jsr303
+ * @author hao_shurni
+ * @since 2011/02/17
+ * @version $Revision$
+ */
+public class MemberValidator implements Validator {
+
+	/**
+	 * ログ
+	 */
+	private static Log log = LogFactory.getLog(MemberValidator.class);
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return MemberAuthForm.class.equals(clazz);
+	}
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void validate(Object target, Errors errors) {
+	}
+
+}
