869044b6291903b35dcea869f781907716c5013d
hao0323@gmail.com <hao0323@gmail.com@fe9ab1a0-4753-0410-a5e7-0b77550f250c>
2011-03-05 14:37:54 +0000

    共通モデルメタ取得IFを追加

--- trunk/src/server/src/main/java/cn/loveapple/service/cool/service/impl/BaseServiceImpl.java
+++ trunk/src/server/src/main/java/cn/loveapple/service/cool/service/impl/BaseServiceImpl.java
@@ -1,9 +1,10 @@
 package cn.loveapple.service.cool.service.impl;
 
-import java.lang.reflect.Method;
+import static cn.loveapple.service.util.service.ServiceUtil.*;
 
 import org.slim3.datastore.CoreAttributeMeta;
 import org.slim3.datastore.Datastore;
+import org.slim3.datastore.ModelMeta;
 
 import com.google.appengine.api.datastore.Key;
 
@@ -20,38 +21,26 @@ public class BaseServiceImpl {
 		
 		Key key = Datastore.put(obj);
 		
-		return queryByKey(key, obj);
+		return queryByKey(key, obj.getClass());
 	}
 	
 
 	/**
-	 * 
 	 * TODO 未試験
+	 * 
+	 * @param <O>
+	 * @param key
+	 * @param clz
+	 * @return
 	 */
-	public <O> O queryByKey(Key key, O obj) {
+	@SuppressWarnings("unchecked")
+	public <O> O queryByKey(Key key, Class clz) {
 		if(key == null){
 			throw new IllegalArgumentException("key is empty.");
 		}
 		
-		StringBuilder metaClass = new StringBuilder(obj.getClass().getName().length());
-		metaClass.append(obj.getClass().getName());
-		metaClass.append("Meta");
-		metaClass.replace(26, 31, "meta");
-		
-		Method getMethod = null;
-		try{
-			getMethod = Class.forName(metaClass.toString()).getMethod("get");
-		}catch (Exception e) {
-			throw new RuntimeException("create meta method class error.", e);
-		}
-		
-		Object meta = null;
-		try {
-			meta = getMethod.invoke(null);
-		} catch (Exception e) {
-			throw new RuntimeException("invoke meta method class error.", e);
-		}
-		
+		ModelMeta meta = getModelMeta(clz.getName());
+				
 		CoreAttributeMeta keyObj = null;
 		try {
 			keyObj = (CoreAttributeMeta) meta.getClass().getField("key").get(meta);
@@ -59,6 +48,6 @@ public class BaseServiceImpl {
 			throw new RuntimeException("get key fields error.", e);
 		}
 		
-		return (O) Datastore.query(obj.getClass()).filter(keyObj.equal(key)).asSingle();
+		return (O) Datastore.query(clz).filter(keyObj.equal(key)).asSingle();
 	}
 }
