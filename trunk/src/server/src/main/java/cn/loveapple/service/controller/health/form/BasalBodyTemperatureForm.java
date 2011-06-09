11839077c2ef9cdd964a9721de3b2a0e7aca77a5
hao0323@gmail.com <hao0323@gmail.com@fe9ab1a0-4753-0410-a5e7-0b77550f250c>
2011-06-09 05:00:04 +0000

    基礎体温操作フォームを追加

--- trunk/src/server/src/main/java/cn/loveapple/service/controller/health/form/BasalBodyTemperatureForm.java
+++ trunk/src/server/src/main/java/cn/loveapple/service/controller/health/form/BasalBodyTemperatureForm.java
@@ -32,12 +32,15 @@
  */
 package cn.loveapple.service.controller.health.form;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
-import org.springframework.format.annotation.NumberFormat;
-import org.springframework.format.annotation.NumberFormat.Style;
+
+import cn.loveapple.service.Constant;
 
 /**
  * 
@@ -50,7 +53,42 @@ import org.springframework.format.annotation.NumberFormat.Style;
  *
  */
 public class BasalBodyTemperatureForm {
+
+	/**
+	 * 性交フラグ
+	 */
+	@Size(max=1)
+	@Pattern(regexp="(" + Constant.FLG_OFF + "|" + Constant.FLG_ON + ")")
+	private String coitusFlg;
 	
+	/**
+	 * 生理期間フラグ
+	 */
+	@Size(max=1)
+	@Pattern(regexp="(" + Constant.FLG_OFF + "|" + Constant.FLG_ON + ")")
+	private String menstruationFlg;
+	
+	/**
+	 * 生理痛フラグ
+	 */
+	@Size(max=1)
+	@Pattern(regexp="(" + Constant.FLG_OFF + "|" + Constant.FLG_ON + ")")
+	private String cysmenorrheaFlg;
+	
+	/**
+	 * おりもの
+	 */
+	@Size(max=1)
+	@Pattern(regexp="(" + Constant.FLG_OFF + "|" + Constant.FLG_ON + ")")
+	private String leukorrhea;
+	
+	/**
+	 * 温度(°C)
+	 */
+	@NotNull
+	@Max(43)
+	@Min(35)
+	private Double temperature;
 	
 	/**
 	 * {@inheritDoc}
@@ -58,5 +96,85 @@ public class BasalBodyTemperatureForm {
 	@Override
 	public String toString(){
 		return ToStringBuilder.reflectionToString(this);
+	}
+
+	/**
+	 * 性交フラグを取得します。
+	 * @return 性交フラグ
+	 */
+	public String getCoitusFlg() {
+	    return coitusFlg;
+	}
+
+	/**
+	 * 性交フラグを設定します。
+	 * @param coitusFlg 性交フラグ
+	 */
+	public void setCoitusFlg(String coitusFlg) {
+	    this.coitusFlg = coitusFlg;
+	}
+
+	/**
+	 * 生理期間フラグを取得します。
+	 * @return 生理期間フラグ
+	 */
+	public String getMenstruationFlg() {
+	    return menstruationFlg;
+	}
+
+	/**
+	 * 生理期間フラグを設定します。
+	 * @param menstruationFlg 生理期間フラグ
+	 */
+	public void setMenstruationFlg(String menstruationFlg) {
+	    this.menstruationFlg = menstruationFlg;
+	}
+
+	/**
+	 * 生理痛フラグを取得します。
+	 * @return 生理痛フラグ
+	 */
+	public String isCysmenorrheaFlg() {
+	    return cysmenorrheaFlg;
+	}
+
+	/**
+	 * 生理痛フラグを設定します。
+	 * @param cysmenorrheaFlg 生理痛フラグ
+	 */
+	public void setCysmenorrheaFlg(String cysmenorrheaFlg) {
+	    this.cysmenorrheaFlg = cysmenorrheaFlg;
+	}
+
+	/**
+	 * おりものを取得します。
+	 * @return おりもの
+	 */
+	public String getLeukorrhea() {
+	    return leukorrhea;
+	}
+
+	/**
+	 * おりものを設定します。
+	 * @param leukorrhea おりもの
+	 */
+	public void setLeukorrhea(String leukorrhea) {
+	    this.leukorrhea = leukorrhea;
+	}
+
+	/**
+	 * 温度(°C)を取得します。
+	 * @return 温度(°C)
+	 */
+	public Double getTemperature() {
+	    return temperature;
+	}
+
+	/**
+	 * 温度(°C)を設定します。
+	 * @param temperature 温度(°C)
+	 */
+	public void setTemperature(Double temperature) {
+	    this.temperature = temperature;
 	}
 }
