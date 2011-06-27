435202775f7c11517f0e8c0784f4b83c8d672a8f
hao0323 <hao0323@gmail.com>
2011-06-27 00:39:54 +0000

    Share project "loveapple.android.bbt" into "https://loveapple.googlecode.com/svn"

---
+++ src/client/android/bbt/src/cn/loveapple/android/bbt/androidActivity.java
@@ -0,0 +1,14 @@
+package cn.loveapple.android.bbt;
+
+import cn.loveapple.client.android.bbt.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class androidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
