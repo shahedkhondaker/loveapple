a2f883ae95c4ea0ad78bd1e3d4f48e0c18d61d6d
hao0323 <hao0323@gmail.com>
2011-07-20 01:26:42 +0000

    ・画面遷移アニメーションを追加 ・ドラック＆ドロップ終了機能を追加

--- src/client/android/bbt/src/cn/loveapple/client/android/bbt/BbtFacadeActivity.java
+++ src/client/android/bbt/src/cn/loveapple/client/android/bbt/BbtFacadeActivity.java
@@ -33,19 +33,12 @@
 package cn.loveapple.client.android.bbt;
 
 import static cn.loveapple.client.android.Constant.*;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -56,14 +49,10 @@ import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-import cn.loveapple.client.android.LoveappleHealthDatabaseOpenHelper;
 import cn.loveapple.client.android.bbt.R.id;
 import cn.loveapple.client.android.bbt.listener.MessagerOnSeekBarChangeListener;
 import cn.loveapple.client.android.bbt.listener.TemperatureSelectedListener;
-import cn.loveapple.client.android.database.TemperatureDao;
 import cn.loveapple.client.android.database.entity.TemperatureEntity;
-import cn.loveapple.client.android.database.impl.TemperatureDaoImpl;
-import cn.loveapple.client.android.util.DateUtil;
 
 /**
  * Loveapple基礎体温(Android版)ファサードアクティビティ
@@ -72,114 +61,125 @@ import cn.loveapple.client.android.util.DateUtil;
  * @version $Revision:$
  * @date $Date:$
  * @id $Id:$
- *
+ * 
  */
 public class BbtFacadeActivity extends BaseActivity implements OnClickListener {
-		
+
 	/**
 	 * 各コンポーネント表示するための初期化を行う
 	 */
 	@Override
-	protected void initView(){
-		
+	protected void initView() {
+
 		// 送信ボタン
-        Button submit = (Button) findViewById(id.submit);
-        submit.setOnClickListener(this);
-		
+		Button submit = (Button) findViewById(id.submit);
+		submit.setOnClickListener(this);
+
 		// 直近の体温情報を取得
 		TemperatureEntity entity = dao.findByDate(today);
 
-		if(entity == null){
+		if (entity == null) {
 			entity = new TemperatureEntity();
 		}
 
-		String[] measureList = getResources().getStringArray(R.array.measureList);
+		String[] measureList = getResources().getStringArray(
+				R.array.measureList);
 		// 体温の初期化
-       initTemperature(entity);
-       
-       // 下り物セレクトボックス初期化
-       SeekBar leukorrhea = (SeekBar) findViewById(id.leukorrhea);
-       TextView leukorrheaViewMsg = (TextView)findViewById(id.leukorrheaViewMsg);
-       leukorrhea.setOnSeekBarChangeListener(new MessagerOnSeekBarChangeListener(leukorrheaViewMsg, measureList));
-       if(entity.getLeukorrhea() == null){
-    	   leukorrhea.setProgress(0);
-       }else{
-    	   leukorrhea.setProgress(Integer.parseInt(entity.getLeukorrhea()));
-       }
-       leukorrheaViewMsg.setText(measureList[leukorrhea.getProgress()]);
+		initTemperature(entity);
 
-       
+		// 下り物セレクトボックス初期化
+		SeekBar leukorrhea = (SeekBar) findViewById(id.leukorrhea);
+		TextView leukorrheaViewMsg = (TextView) findViewById(id.leukorrheaViewMsg);
+		leukorrhea
+				.setOnSeekBarChangeListener(new MessagerOnSeekBarChangeListener(
+						leukorrheaViewMsg, measureList));
+		if (entity.getLeukorrhea() == null) {
+			leukorrhea.setProgress(0);
+		} else {
+			leukorrhea.setProgress(Integer.parseInt(entity.getLeukorrhea()));
+		}
+		leukorrheaViewMsg.setText(measureList[leukorrhea.getProgress()]);
+
 		// 生理出血量
-       SeekBar menstruationLevel = (SeekBar) findViewById(id.menstruationLevel);
-       TextView menstruationLevelViewMsg = (TextView)findViewById(id.menstruationLevelViewMsg);
-       menstruationLevel.setOnSeekBarChangeListener(new MessagerOnSeekBarChangeListener(menstruationLevelViewMsg, measureList));
-       if(entity.getMenstruationLevel() == null){
-    	   menstruationLevel.setProgress(0);
-       }else{
-    	   menstruationLevel.setProgress(Integer.parseInt(entity.getMenstruationLevel()));
-       }
-       menstruationLevelViewMsg.setText(measureList[menstruationLevel.getProgress()]);
-       
-	       
+		SeekBar menstruationLevel = (SeekBar) findViewById(id.menstruationLevel);
+		TextView menstruationLevelViewMsg = (TextView) findViewById(id.menstruationLevelViewMsg);
+		menstruationLevel
+				.setOnSeekBarChangeListener(new MessagerOnSeekBarChangeListener(
+						menstruationLevelViewMsg, measureList));
+		if (entity.getMenstruationLevel() == null) {
+			menstruationLevel.setProgress(0);
+		} else {
+			menstruationLevel.setProgress(Integer.parseInt(entity
+					.getMenstruationLevel()));
+		}
+		menstruationLevelViewMsg.setText(measureList[menstruationLevel
+				.getProgress()]);
+
 		// セックス
-		CheckBox coitus = (CheckBox)findViewById(id.coitus);
+		CheckBox coitus = (CheckBox) findViewById(id.coitus);
 		coitus.setChecked(FLG_ON.equals(entity.getCoitusFlg()));
-		
+
 		// 生理
-		CheckBox menstruation = (CheckBox)findViewById(id.menstruation);
+		CheckBox menstruation = (CheckBox) findViewById(id.menstruation);
 		menstruation.setChecked(FLG_ON.equals(entity.getMenstruationFlg()));
-		
+
 		// 生理痛
 		CheckBox dysmenorrhea = (CheckBox) findViewById(id.dysmenorrhea);
 		dysmenorrhea.setChecked(FLG_ON.equals(entity.getDysmenorrheaFlg()));
-        
+
 		TextView headMsg = (TextView) findViewById(id.headMsg);
-		headMsg.setText(String.format(getText(R.string.hello).toString(), todayDate));
+		headMsg.setText(String.format(getText(R.string.hello).toString(),
+				todayDate));
 	}
-	
+
 	/**
 	 * 体温セレクトボックスを初期化
+	 * 
 	 * @param entity
 	 */
-	private void initTemperature(TemperatureEntity entity){
+	private void initTemperature(TemperatureEntity entity) {
 		// 温度セレクトボックスの初期化
 		Spinner temperature = (Spinner) findViewById(id.temperature);
-		ArrayAdapter<String> tempperatureAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
-        tempperatureAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        int selectionPoint = 0;
-        for(int i = 0; i < temperatureList.size(); i++){
-        	String temperatureValue = temperatureList.get(i);
-        	tempperatureAdapter.add(temperatureValue);
-        	if(entity.getTemperature() != null){
-        		if(entity.getTemperature().intValue() == Integer.parseInt(temperatureValue)){
-        			selectionPoint = i;
-        		}
-        	}
-        }
-        temperature.setAdapter(tempperatureAdapter);        
-        // スピナーのアイテムが選択された時に呼び出されるコールバックリスナーを登録します
-        temperature.setOnItemSelectedListener(new TemperatureSelectedListener(this));
-        temperature.setSelection(selectionPoint);
-        
-        if(entity.getTemperature() == null){
-        	return ;
-        }        
-        // 体温テキストを初期化
-        EditText temperatureText = (EditText) findViewById(id.temperatureText);
-        double value = entity.getTemperature() == null? 0 : entity.getTemperature();
-        if(value == 0.0){
-        	temperatureText.setText("0");
-        	return ;
-        }
-        value = (value - entity.getTemperature().intValue())*100;
-        temperatureText.setText(String.valueOf(Math.abs(value)));
- 	}
-	
+		ArrayAdapter<String> tempperatureAdapter = new ArrayAdapter<String>(
+				this, android.R.layout.simple_spinner_item);
+		tempperatureAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		int selectionPoint = 0;
+		for (int i = 0; i < temperatureList.size(); i++) {
+			String temperatureValue = temperatureList.get(i);
+			tempperatureAdapter.add(temperatureValue);
+			if (entity.getTemperature() != null) {
+				if (entity.getTemperature().intValue() == Integer
+						.parseInt(temperatureValue)) {
+					selectionPoint = i;
+				}
+			}
+		}
+		temperature.setAdapter(tempperatureAdapter);
+		// スピナーのアイテムが選択された時に呼び出されるコールバックリスナーを登録します
+		temperature.setOnItemSelectedListener(new TemperatureSelectedListener(
+				this));
+		temperature.setSelection(selectionPoint);
 
-    /**
-     * 
-     * {@inheritDoc}
-     */
+		if (entity.getTemperature() == null) {
+			return;
+		}
+		// 体温テキストを初期化
+		EditText temperatureText = (EditText) findViewById(id.temperatureText);
+		double value = entity.getTemperature() == null ? 0 : entity
+				.getTemperature();
+		if (value == 0.0) {
+			temperatureText.setText("0");
+			return;
+		}
+		value = (value - entity.getTemperature().intValue()) * 100;
+		temperatureText.setText(String.valueOf(Math.abs(value)));
+	}
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void onClick(View v) {
 		save();
@@ -187,128 +187,170 @@ public class BbtFacadeActivity extends BaseActivity implements OnClickListener {
 		Intent intent = new Intent();
 		intent.setClassName(this, BbtChartActivity.class.getName());
 		startActivity(intent);
-		finish();
 	}
-	
-	private TemperatureEntity save(){
+
+	private TemperatureEntity save() {
 		TemperatureEntity result = null;
-		try{
+		try {
 			dao.save(createEntity());
 			result = dao.findByDate(today);
-			
-			Toast.makeText(this, "submit!!" 
-					+ "date:" + result, Toast.LENGTH_LONG).show();
-			
+
+			Toast.makeText(this, "submit!!" + "date:" + result,
+					Toast.LENGTH_LONG).show();
+
 			return result;
-		}catch (Exception e) {
-			Toast.makeText(this, "Exception!!" 
-					+ e.getMessage(), Toast.LENGTH_LONG).show();
+		} catch (Exception e) {
+			Toast.makeText(this, "Exception!!" + e.getMessage(),
+					Toast.LENGTH_LONG).show();
 		}
 		return result;
 	}
-	
+
 	/**
 	 * 
 	 * @return
 	 */
-	private TemperatureEntity createEntity(){
+	private TemperatureEntity createEntity() {
 		TemperatureEntity entity = new TemperatureEntity();
 		EditText temperatureText = (EditText) findViewById(id.temperatureText);
 		Spinner temperature = (Spinner) findViewById(id.temperature);
-		CheckBox coitus = (CheckBox)findViewById(id.coitus);
-		CheckBox menstruation = (CheckBox)findViewById(id.menstruation);
+		CheckBox coitus = (CheckBox) findViewById(id.coitus);
+		CheckBox menstruation = (CheckBox) findViewById(id.menstruation);
 		CheckBox dysmenorrhea = (CheckBox) findViewById(id.dysmenorrhea);
 		SeekBar leukorrhea = (SeekBar) findViewById(id.leukorrhea);
 		SeekBar menstruationLevel = (SeekBar) findViewById(id.menstruationLevel);
 
 		// 温度の選択値
 		StringBuilder temperatureValue = new StringBuilder(10);
-		temperatureValue.append(temperatureList.get(temperature.getSelectedItemPosition()));
+		temperatureValue.append(temperatureList.get(temperature
+				.getSelectedItemPosition()));
 		temperatureValue.append('.');
 		temperatureValue.append(temperatureText.getText());
-	
-		
-		entity.setCoitusFlg(coitus.isChecked()?FLG_ON:FLG_OFF);
+
+		entity.setCoitusFlg(coitus.isChecked() ? FLG_ON : FLG_OFF);
 		entity.setTemperature(new Double(temperatureValue.toString()));
 		entity.setDate(today);
-		entity.setMenstruationFlg(menstruation.isChecked()?FLG_ON:FLG_OFF);
-		entity.setDysmenorrheaFlg(dysmenorrhea.isChecked()?FLG_ON:FLG_OFF);
+		entity.setMenstruationFlg(menstruation.isChecked() ? FLG_ON : FLG_OFF);
+		entity.setDysmenorrheaFlg(dysmenorrhea.isChecked() ? FLG_ON : FLG_OFF);
 		entity.setLeukorrhea(String.valueOf(leukorrhea.getProgress()));
-		entity.setMenstruationLevel(String.valueOf(menstruationLevel.getProgress()));
-		
-		
+		entity.setMenstruationLevel(String.valueOf(menstruationLevel
+				.getProgress()));
+
 		return entity;
 	}
-	
+
 	/**
 	 * 
 	 * {@inheritDoc}
 	 */
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item){
+	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case MENU_HELP:
 			Toast.makeText(this, "HELP", Toast.LENGTH_LONG).show();
 
-			
 			return true;
 		case MENU_OPT:
 			Toast.makeText(this, "Option", Toast.LENGTH_LONG).show();
-			
+
 			Intent intent = new Intent();
 			intent.setClassName(this, BbtSetting.class.getName());
 			startActivity(intent);
 			return true;
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * 
 	 * {@inheritDoc}
 	 */
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu){
-		menu.add(0, MENU_OPT, 0, getText(R.string.setting))
-			.setIcon(android.R.drawable.ic_menu_preferences);
-		menu.add(0, MENU_HELP, 0, getText(R.string.help))
-			.setIcon(android.R.drawable.ic_menu_help);
-		
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0, MENU_OPT, 0, getText(R.string.setting)).setIcon(
+				android.R.drawable.ic_menu_preferences);
+		menu.add(0, MENU_HELP, 0, getText(R.string.help)).setIcon(
+				android.R.drawable.ic_menu_help);
+
 		return true;
 	}
-	
+
 	/**
 	 * 表示/非表示の初期化<br>
 	 * <b>ルール：</b><br>
 	 * 1．生理痛、出血量、下り物がオプションで設定される場合のみ表示する。
-	 * 2．オプションで表示設定される場合、生理を選択される場合、生理痛、出血量が表示される。
-	 * 3．生理を選択される場合、下り物が非表示する。
+	 * 2．オプションで表示設定される場合、生理を選択される場合、生理痛、出血量が表示される。 3．生理を選択される場合、下り物が非表示する。
 	 * 
 	 */
 	@Override
-	protected void initVisibility(){
-		
+	protected void initVisibility() {
+
 	}
-	
-	@Override
-	protected void onSaveInstanceState(Bundle outState){
-		super.onSaveInstanceState(outState);
-		save();
-	}
-	
-	@Override
-	protected void onRestoreInstanceState(Bundle savedInstanceState){
-		super.onRestoreInstanceState(savedInstanceState);
-	}
+
 	/**
 	 * 
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void onDestroy(){ 
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		save();
+	}
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+	}
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			downX = event.getX();
+			downY = event.getY();
+			break;
+		case MotionEvent.ACTION_UP:
+			break;
+		case MotionEvent.ACTION_MOVE:
+			break;
+		case MotionEvent.ACTION_CANCEL:
+			break;
+		}
+		if(event.getAction() == MotionEvent.ACTION_OUTSIDE){
+
+			Log.d(LOG_TAG,
+			        "ouside");
+		}
+		if(event.getAction() == MotionEvent.ACTION_UP && 100 <= Math.abs(downX - event.getX())){
+
+			Log.d(LOG_TAG,
+			        "true: downX = " + downX + ", " +
+			        "downY = " + downY + ", x = " + event.getX());
+			finish();
+			return super.onTouchEvent(event);
+			
+		}
+		return super.onTouchEvent(event);
+
+	}
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onDestroy() {
 		super.onDestroy();
-		if(dao != null){
+		if (dao != null) {
 			dao.destory();
 		}
 	}
