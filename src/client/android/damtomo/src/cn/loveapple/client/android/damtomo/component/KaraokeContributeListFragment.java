76f3a5394f4092b4dbd4b0f44f398db2f6cc0e64
hao0323 <hao0323@gmail.com>
2011-09-14 23:19:51 +0000

    通信コンポーネント作成中

--- src/client/android/damtomo/src/cn/loveapple/client/android/damtomo/component/KaraokeContributeListFragment.java
+++ src/client/android/damtomo/src/cn/loveapple/client/android/damtomo/component/KaraokeContributeListFragment.java
@@ -1,3 +1,35 @@
+/*
+ * $HeadURL$
+ * $Author$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ *
+ * Copyright (C) 2008 by loveapple.sourceforge.jp
+ *
+ * All copyright notices regarding loveapple and loveapple CoreLib
+ * MUST remain intact in the scripts, documents and source code.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Correspondence and Marketing Questions can be sent to:
+ * info at loveapple
+ *
+ * @author: loveapple
+ */
 package cn.loveapple.client.android.damtomo.component;
 
 import android.app.FragmentTransaction;
@@ -8,8 +40,15 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import cn.loveapple.client.android.damtomo.R;
-import cn.loveapple.client.android.damtomo.example.Shakespeare;
 
+/**
+ * 
+ * @author loveapple
+ * @version $Revision$
+ * @date $Date$
+ * @id $Id$
+ *
+ */
 public class KaraokeContributeListFragment extends ListFragment {
 	boolean mDualPane;
     int mCurCheckPosition = 0;
@@ -20,7 +59,7 @@ public class KaraokeContributeListFragment extends ListFragment {
 
         // Populate list with our static array of titles.
         setListAdapter(new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_activated_1, Shakespeare.TITLES));
+                android.R.layout.simple_list_item_activated_1, getResources().getStringArray(R.array.karaokeMovieList)));
 
         // Check to see if we have a frame in which to embed the details
         // fragment directly in the containing UI.
