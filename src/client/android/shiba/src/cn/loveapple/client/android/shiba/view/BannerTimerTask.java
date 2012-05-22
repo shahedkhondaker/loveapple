93a762ece877f58fc5abd0d79c1bb77e00efe724
hao0323 <hao0323@gmail.com>
2012-05-22 01:09:54 +0000

    パッケージ名修正

---
+++ src/client/android/shiba/src/cn/loveapple/client/android/shiba/view/BannerTimerTask.java
@@ -0,0 +1,30 @@
+package cn.loveapple.client.android.shiba.view;
+
+import static cn.loveapple.client.android.Constant.*;
+
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.util.Log;
+
+public class BannerTimerTask extends TimerTask {
+	
+	private Activity activity;
+	private long startTime;
+	
+	public BannerTimerTask(Activity activity){
+		startTime = System.currentTimeMillis();
+		this.activity = activity;
+	}
+
+	@Override
+	public void run() {
+		long millis = System.currentTimeMillis() - startTime;
+		int seconds = (int) (millis / 1000);
+		int minutes = seconds / 60;
+		seconds     = seconds % 60;
+		
+		Log.d(LOG_TAG, String.format("Timmer: %d:%02d", minutes, seconds));
+	}
+
+}
